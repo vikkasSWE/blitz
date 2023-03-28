33e625605e3a64b8d1e80da307ecce2b7742e3a3
Viktor Kasimir <kasimir.viktor@gmail.com>
2023-03-28 08:18:19 +0200

    world tilemap

--- projs/client/src/main.rs
+++ projs/client/src/main.rs
@@ -2,9 +2,12 @@ use std::path::Path;
 
 use bevy::{
     core_pipeline::{bloom::BloomSettings, tonemapping::Tonemapping},
+    input::common_conditions::input_toggle_active,
     prelude::*,
 };
-use bincode::de;
+use bevy_ecs_ldtk::prelude::*;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
+
 use blitz_common::{panic_on_error_system, PlayerCommand};
 use exit::exit_system;
 
@@ -19,16 +22,27 @@ mod resources;
 
 fn main() {
     let mut app = App::new();
-    app.add_plugins(DefaultPlugins.set(WindowPlugin {
-        primary_window: Some(Window {
-            title: "Blitz Client".to_string(),
-            ..Default::default()
-        }),
-        ..Default::default()
-    }));
+    app.add_plugins(
+        DefaultPlugins
+            .set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "Blitz Client".to_string(),
+                    ..Default::default()
+                }),
+                ..Default::default()
+            })
+            .set(ImagePlugin::default_nearest()),
+    );
 
     app.add_plugin(ClientPlayerPlugin);
     app.add_plugin(ClientNetworkPlugin);
+
+    app.add_plugin(LdtkPlugin)
+        .add_plugin(
+            WorldInspectorPlugin::default().run_if(input_toggle_active(true, KeyCode::Escape)),
+        )
+        .insert_resource(LevelSelection::Index(0))
+        .register_ldtk_entity::<MyBundle>("MyEntityIdentifier");
 
     app.add_startup_system(setup);
 
@@ -59,4 +73,24 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         player: asset_server.load(asset_path.join(PLAYER_SPRITE)),
         player_laser: asset_server.load(asset_path.join(PLAYER_LASER_SPRITE)),
     });
+
+    commands.spawn(LdtkWorldBundle {
+        ldtk_handle: asset_server.load(asset_path.join("world.ldtk")),
+        ..Default::default()
+    });
+}
+
+#[derive(Default, Component)]
+struct ComponentA;
+
+#[derive(Default, Component)]
+struct ComponentB;
+
+#[derive(Bundle, LdtkEntity)]
+pub struct MyBundle {
+    a: ComponentA,
+    b: ComponentB,
+    #[sprite_sheet_bundle]
+    #[bundle]
+    sprite_bundle: SpriteSheetBundle,
 }
